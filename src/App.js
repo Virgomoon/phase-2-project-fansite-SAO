e6c2683fc9012847c256a05020bed9744280eddd
Joseph Guillory <jagmanx99@yahoo.com>
2022-06-21 01:12:03 -0700

    Character page beta

--- src/App.js
+++ src/App.js
@@ -14,12 +14,19 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const [searchData, setSearchData] = useState([])
+  const [firstChar, setFirstChar] = useState([])
     
     useEffect(() =>{
         fetch("https://api.jikan.moe/v4/anime?q=sword art online&sfw")
         .then((res)=> res.json())
-        .then((data)=> setSearchData(data.data), console.log(searchData))
+        .then((data)=> setSearchData(data.data))
     }, []);
+
+    useEffect(() =>{
+      fetch("https://api.jikan.moe/v4/anime/11757/characters")
+      .then((res)=> res.json())
+      .then((data)=> setFirstChar(data.data))
+  }, []);
 
   return (
     <div className="App">
@@ -27,7 +34,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home isLoggedIn={isLoggedIn} 
         searchData={searchData} />} />
-        <Route path='/Characters' element={<Characters />} />
+        <Route path='/Characters' element={<Characters firstChar={firstChar} />} />
         <Route path='/Reviews' element={<Reviews />} />
         <Route path='/About' element={<About />} />
         <Route path='/Login' element={<Login setIsLoggedIn={setIsLoggedIn} />} />
--- src/Characters.js
+++ src/Characters.js
@@ -1,8 +1,22 @@
 import React from "react";
 
-function Characters(){
+function Characters({firstChar}){
 
-    return <p>Characters</p>
+    const charData = firstChar.map((item)=> {
+        return(
+            <div key={item.character.mal_id}>
+                <img src={item.character.images.jpg.image_url}></img>
+                <h3>{item.character.name}</h3>
+            </div>
+        )
+        })
+
+    return (
+        <div>
+            <p>Characters</p>
+            {charData}
+        </div>
+        )
 }
 
 export default Characters;
